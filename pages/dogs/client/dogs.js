d3475b012a893e6f2fc15d55be7c76b31424524a
YulingHu <hyl@HYLdeMacBook-Air.local>
2016-06-28 01:00:56 -0400

    PA02-new

--- pages/dogs/client/dogs.js
+++ pages/dogs/client/dogs.js
@@ -1,5 +1,17 @@
 Template.dogs.helpers({
-	dogsearch: function(){return DogSearch.find({}, {sort:{dog_popularity:-1}});}
+	dogsearch: function(){
+		return DogSearch.find({}, {sort:{dog_popularity:-1}});
+	},
+
+	theColor: function(){
+    const instance = Template.instance();
+    return instance.state.get("color");
+  },
+
+  theCounter: function(){
+    const instance = Template.instance();
+    return instance.state.get("counter");
+  },
 })
 
 Template.dogs.events({
@@ -14,6 +26,23 @@ Template.dogs.events({
 		else{
 			Meteor.call("dogupdate",breed);
 		}
+	},
 
-	}
+	"change .js-color": function(event,instance){
+    const c = instance.$(".js-color").val();
+    instance.state.set("color",c);
+  },
+
+  "click .js-pushme": function(event,instance){
+    const counter = instance.state.get("counter");
+    instance.state.set("counter",1+counter);
+  },
 })
+
+Template.dogs.onCreated(function() {
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    color:"bg-info",
+    counter:0,
+  });
+});
